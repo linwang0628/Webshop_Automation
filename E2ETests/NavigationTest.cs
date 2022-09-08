e5d5ff8edabdd8e3a2e5a8e31110584ce91b6eb8
linwang0628 <31600358+linwang0628@users.noreply.github.com>
2022-09-08 20:01:27 +1000

    Add project files.

---
+++ E2ETests/NavigationTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop_Automation.E2ETests
+{
+    internal class NavigationTest
+    {
+    }
+}
